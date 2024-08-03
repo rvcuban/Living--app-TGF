e78ff547f20f369392c9b56b5037d2a9ddb83f4d
rvcuban <78472187+rvcuban@users.noreply.github.com>
2024-08-03 07:31:59 +0200

    crear paginas y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>APP</h1>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/sign-in" element={<SingIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Home />} />
+
+    </Routes>
+  </BrowserRouter>
 }
