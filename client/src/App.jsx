a1a4d60548cea6bf5e80da94b75eb99146476d0a
rvcuban <78472187+rvcuban@users.noreply.github.com>
2024-08-04 11:34:13 +0200

    funcionalidad de registro completado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,9 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import SingIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
-import SignUp from "./pages/SignUp";
+
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp"; 
 
 
 export default function App() {
