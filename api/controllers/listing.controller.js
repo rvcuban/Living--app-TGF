1a89523b48951e541ced4f47f0d183a6a31fc67b
rvcuban <78472187+rvcuban@users.noreply.github.com>
2024-08-24 11:42:00 +0200

    termine la funcionalidad de delete listing(eliminar propiedades) de un usuario

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js';
+import { errorHandle } from '../utils/error.js';
 
 
 export const createListing = async (req, res, next) => {
@@ -8,4 +9,26 @@ export const createListing = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+
+export const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandle(404, 'Listing not found'));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandle(401, 'Solo puedes borrrar tus propiedades'));
+  }
+
+  try {
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json('la Propiedad ha sido eliminada');
+  } catch (error) {
+    next(error);
+
+  }
+
 };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 
 //verificamos si esta autentificado y despues le dejamos crear el listing 
 router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
