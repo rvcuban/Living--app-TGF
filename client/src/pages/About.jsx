f4eb6b7021f117631fbbcf928b063290998a559d
rvcuban <78472187+rvcuban@users.noreply.github.com>
2024-09-19 07:15:59 +0200

    about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,8 +3,10 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+     <h1 className='text-3xl font-bold text-slate-800'>About Living App TFG</h1>
+     <p className='mb-4 text-slate-800'>Este es mi tfg y mi primera startup</p>
+     <p className='mb-4 text-slate-800'>coooking</p>
     </div>
   )
 }
