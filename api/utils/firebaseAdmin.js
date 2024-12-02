0b38502b654707e2283564640faa1a184e41f770
rvcuban <78472187+rvcuban@users.noreply.github.com>
2024-12-02 17:33:10 +0100

    anotrher change to make it work

--- api/utils/firebaseAdmin.js
+++ api/utils/firebaseAdmin.js
@@ -4,26 +4,30 @@ import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import fs from 'fs';
+
 
 // Definir __dirname en módulos ES
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // Ruta al Secret File montado por Render
-const serviceAccountPath = '/etc/secrets/serviceAccountKey.json';
+//const serviceAccountPath = '/etc/secrets/serviceAccountKey.json';
 
-if (!fs.existsSync(serviceAccountPath)) {
+/*if (!fs.existsSync(serviceAccountPath)) {
     console.error(`El archivo de servicio de Firebase no se encontró en la ruta: ${serviceAccountPath}`);
     process.exit(1); // Salir de la aplicación si el archivo no está presente
-  }
+  }*/
 
-  const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
+  //const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 // Leer las credenciales del archivo JSON
 /*const serviceAccount = JSON.parse(
   readFileSync(join(__dirname, 'secrets/serviceAccountKey.json'), 'utf8')
 );*/
+
+
+// Leer las credenciales desde la variable de entorno
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 // Inicializar la aplicación con las credenciales del servicio
 admin.initializeApp({
