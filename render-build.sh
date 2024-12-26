8f8914731123295ef71bbd8f25a67db3f1244598
rvcuban <78472187+rvcuban@users.noreply.github.com>
2024-12-26 10:03:45 +0100

    se me olvido un comentario

--- render-build.sh
+++ render-build.sh
@@ -5,9 +5,9 @@ set -o errexit
 #Install dependencies
 npm install
 
-#Uncomment this line if you need to build your project
+Uncomment this line if you need to build your project
 npm run build
-Ensure the Puppeteer cache directory exists
+#Ensure the Puppeteer cache directory exists
 PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
 mkdir -p $PUPPETEER_CACHE_DIR
 
