dca975445541e73bc00da96fa5a31efa0b80e36a
rvcuban <78472187+rvcuban@users.noreply.github.com>
2025-03-10 10:24:16 +0100

    modificado el home

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -36,10 +36,16 @@ module.exports = {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        fadeIn: {
+                   '0%': { opacity: 0, transform: 'translateY(-10px)' },
+                   '100%': { opacity: 1, transform: 'translateY(0)' }
+                 }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'pulse-fast': 'pulse 0.5s cubic-bezier(0,0,0.2,1) forwards',
+        'fade-in': 'fadeIn 0.3s ease-out forwards',
       },
     },
   },
