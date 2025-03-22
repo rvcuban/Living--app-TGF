49a2bb8882d54d496a39e35995d7955ec7793daf
rvcuban <78472187+rvcuban@users.noreply.github.com>
2025-03-22 22:35:12 +0100

    cambios en el copdifo para el segundo deploy

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,53 +1,51 @@
 
-const { fontFamily } = require("tailwindcss/defaultTheme")
+import { fontFamily } from "tailwindcss/defaultTheme"
 
-module.exports = {
-  darkMode: 'class',
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
-    extend: {
-      colors: {
-        // Definiciones de color
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      fontFamily: {
-        sans: ['Poppins', ...fontFamily.sans],
-      },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-        fadeIn: {
-                   '0%': { opacity: 0, transform: 'translateY(-10px)' },
-                   '100%': { opacity: 1, transform: 'translateY(0)' }
-                 }
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        'pulse-fast': 'pulse 0.5s cubic-bezier(0,0,0.2,1) forwards',
-        'fade-in': 'fadeIn 0.3s ease-out forwards',
-      },
+export const darkMode = 'class'
+export const content = [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}',
+]
+export const theme = {
+  container: {
+    center: true,
+    padding: '2rem',
+    screens: {
+      '2xl': '1400px',
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  extend: {
+    colors: {
+      // Definiciones de color
+    },
+    borderRadius: {
+      lg: 'var(--radius)',
+      md: 'calc(var(--radius) - 2px)',
+      sm: 'calc(var(--radius) - 4px)',
+    },
+    fontFamily: {
+      sans: ['Poppins', ...fontFamily.sans],
+    },
+    keyframes: {
+      'accordion-down': {
+        from: { height: '0' },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: '0' },
+      },
+      fadeIn: {
+        '0%': { opacity: 0, transform: 'translateY(-10px)' },
+        '100%': { opacity: 1, transform: 'translateY(0)' }
+      }
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
+      'pulse-fast': 'pulse 0.5s cubic-bezier(0,0,0.2,1) forwards',
+      'fade-in': 'fadeIn 0.3s ease-out forwards',
+    },
+  },
 }
+export const plugins = [require('tailwindcss-animate')]
